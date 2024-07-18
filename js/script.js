10beb46322112bfd6a598a8d9db07e7d41de2bc9
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-07-18 12:12:06 +0200

    aggiusto milestone 2 partendo da 0 su js

--- js/script.js
+++ js/script.js
@@ -6,22 +6,3 @@ const images = [
     'img/04.webp',
     'img/05.webp'
 ];
-
-// Selezioniamo il contenitore del carosello
-const carousel = document.querySelector('.carousel');
-let activeIndex = 0; // Indice dell'immagine attiva
-
-// Creare il markup usando un ciclo for
-let carouselContent = '';
-for (let i = 0; i < images.length; i++) {
-    console.log(`Caricando immagine: ${images[i]}`); // Log del percorso dell'immagine
-    carouselContent += `
-        <img src="${images[i]}" class="carousel-image ${i === 0 ? 'active' : ''}">
-    `;
-}
-
-// Inserire il contenuto nel carosello
-carousel.innerHTML = carouselContent;
-
-
-console.log('Immagini inserite nel carosello:', images);
